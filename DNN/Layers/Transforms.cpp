c613bd697a0191eb5e56b9967c4566e2d869d09d
Mazen Anastas <lorenzo.mallerco@gmail.com>
2020-10-29 19:14:39 +0700

    Add: Added activation functions for DNNs

--- DNN/Layers/Transforms.cpp
+++ DNN/Layers/Transforms.cpp
@@ -17,6 +17,100 @@ long double max(long double* data, const int count)
 	return max;
 }
 
+long double coth(long double x)
+{
+	return cosh(x) / sinh(x);
+}
+
+long double sech(long double x)
+{
+	return 1 / cosh(x);
+}
+
+long double csch(long double x)
+{
+	return 1 / sinh(x);
+}
+
+long double acoth(long double x)
+{
+	return 1 / 2 * log((x + 1) / (x - 1));
+}
+
+long double asech(long double x)
+{
+	return log(1 / x + sqrt(1 / (x * x) - 1));
+}
+
+long double acsch(long double x) 
+{
+	return log(1 / x + sqrt(1 / (x * x) + 1));
+}
+
+long double sinhDerivative(long double x)
+{
+	return cosh(x);
+}
+
+long double coshDerivative(long double x)
+{
+	return sinh(x);
+}
+
+long double tanhDerivative(long double x) 
+{
+	long double t = tanh(x);
+	return 1 - (t * t);
+}
+
+long double cothDerivative(long double x)
+{
+	long double c = coth(x);
+	return 1 - (c * c);
+}
+
+long double sechDerivative(long double x)
+{
+	return -tanh(x) * sech(x);
+}
+
+long double cschDerivative(long double x)
+{
+	return -coth(x) * csch(x);
+}
+
+long double asinhDerivative(long double x)
+{
+	return 1 / sqrt(x * x + 1);
+}
+
+long double acoshDerivative(long double x)
+{
+	return 1 / sqrt(x * x - 1);
+}
+
+long double atanhDerivative(long double x)
+{
+	return 1 / (1 - x * x);
+}
+
+long double acothDerivative(long double x)
+{
+	return 1 / (1 - x * x);
+}
+
+long double asechDerivative(long double x)
+{
+	return 1 /(x * sqrt(1 - x * x));
+}
+
+long double acschDerivative(long double x)
+{
+	return 1 / (abs(x) * sqrt(x * x + 1));
+}
+
+/* Activation Functions */
+
 long double* softmax(long double* data, const int count)
 {
 	long double sum = 0, 
@@ -31,6 +125,25 @@ long double* softmax(long double* data, const int count)
 	return result;
 }
 
+Matrix& softmaxDerivative(Matrix& matrix)
+{
+	if (!matrix.isSquare())
+		throw NotSquareMatrixError();
+	/* Python:
+		s = arr.reshape(-1, 1);
+		result = np.diagflat(s) -  np.dot(s, s.T)
+	*/
+	Matrix* s = &matrix.flatten();
+	Matrix* sT = &s->transpose();
+	Matrix* left = &Matrix::diagonal(matrix.getData(), matrix.count);
+	Matrix* right = &s->times(*sT);
+	delete s;
+	delete sT;
+	Matrix* result = &left->minus(*right);
+	delete left;
+	return *result;
+}
+
 long double sigmoid(long double x)
 {
 	return x / (1 + abs(x));
@@ -41,3 +154,109 @@ long double sigmoidDerivative(long double x)
 	return x * (1 - x);
 }
 
+long double relu(long double x)
+{
+	return x > 0 ? x : 0;
+}
+
+long double reluDerivative(long double x)
+{
+	return x > 0 ? 1 : 0;
+}
+
+long double leakyRelu(long double x, long double alpha)
+{
+	return x > 0 ? x : alpha * x;
+}
+
+long double leakyReluDerivative(long double x, long double alpha)
+{
+	return x > 0 ? 1 : alpha;
+}
+
+long double relu6(long double x)
+{
+	if (x <= 0)
+		return 0; 
+	else if (x >= 6)
+		return 6;
+	return x;
+}
+
+long double relu6Derivative(long double x)
+{
+	return (x <= 0 || x >= 6) ? 0 : 1;
+}
+
+long double elu(long double x, long double alpha)			
+{
+	return x > 0 ? x : alpha * (exp(x) - 1);
+}
+
+long double eluDerivative(long double x, long double alpha)
+{
+	return x > 0 ? 1 : elu(x, alpha) + alpha;
+}
+
+long double selu(long double x, long double alpha, long double lambda)
+{
+	return lambda * (x > 0 ? x : (alpha * exp(x) - alpha));
+}
+
+long double seluDerivative(long double x, long double alpha, long double lambda)
+{
+	return lambda * (x > 0 ? 1 : (alpha * exp(x)));
+}
+
+long double gelu(long double x)
+{
+	return 0.5 * x * (1 + tanh(sqrt(2 / 3.14) * (x + 0.044715 * x * x * x)));
+}
+
+long double geluDerivative(long double x)
+{
+	long double s = sech(0.0356774 * x * x * x + 0.797885 * x);
+	return 0.5 * tanh(0.0356774 * x * x * x + 0.797885 * x) + (0.0535161 * x * x * x + 0.398942 * x) * (s * s) + 0.5;
+}
+
+long double cube(long double x)
+{
+	return x * x * x;
+}
+
+long double cubeDerivative(long double x)
+{
+	return 3 * x * x;
+}
+
+long double swish(long double x)
+{
+	return x * sigmoid(x);
+}
+
+long double swishDerivative(long double x)
+{
+	long double s = sigmoid(x);
+	return 2 * s - s * s;
+}
+
+long double softsign(long double x)
+{
+	return x / (1 + abs(x));
+}
+
+long double softsignDerivative(long double x)
+{
+	long double a = abs(x);
+	return (1 + a - x * x / a) / ((1 + a) * (1 + a));
+}
+
+long double softplus(long double x)
+{
+	return log(1 + exp(x));
+}
+
+long double softplusDerivative(long double x)
+{
+	return 1 / (1 + exp(-x));
+}
--- DNN/Layers/Transforms.h
+++ DNN/Layers/Transforms.h
@@ -1,9 +1,90 @@
 #ifndef TRANSFORMS_H
 #define TRANSFORMS_H
 
+/* References : 
+https://en.wikipedia.org/wiki/Hyperbolic_functions
+https://mlfromscratch.com/activation-functions-explained/#/  
+*/
+
+/* Utility */
 long double max(long double* data, const int count);
-long double* softmax(long double* data, const int count);
+long double coth(long double x);
+long double sech(long double x);
+long double csch(long double x);
+long double acoth(long double x);
+long double asech(long double x);
+long double acsch(long double x);
+long double sinhDerivative(long double x);
+long double coshDerivative(long double x);
+long double tanhDerivative(long double x);
+long double cothDerivative(long double x);
+long double sechDerivative(long double x);
+long double cschDerivative(long double x);
+long double asinhDerivative(long double x);
+long double acoshDerivative(long double x);
+long double atanhDerivative(long double x);
+long double acothDerivative(long double x);
+long double asechDerivative(long double x);
+long double acschDerivative(long double x);
+/* Sigmoid */
 long double sigmoid(long double x);
 long double sigmoidDerivative(long double x);
+/* ReLU */
+long double relu(long double x);
+long double reluDerivative(long double x);
+/* LeakyReLU */
+long double leakyRelu(long double x, long double ratio = 0.3);
+long double leakyReluDerivative(long double x, long double ratio = 0.3);
+/* Relu6 */
+long double relu6(long double x);
+long double relu6Derivative(long double x);
+/* ELU */
+long double elu(long double x, long double alpha = 0.3);					// alpha usually belongs to [0.1, 0.3]
+long double eluDerivative(long double x, long double alpha = 0.3);
+/* Scaled ELU */
+long double selu(long double x, long double alpha = 1.6732632423543772848170429916717, long double lambda = 1.0507009873554804934193349852946);
+long double seluDerivative(long double x, long double alpha = 1.6732632423543772848170429916717, long double lambda = 1.0507009873554804934193349852946);
+/* Gaussian ELU */
+long double gelu(long double x);
+long double geluDerivative(long double x);
+/* Cube */
+long double cube(long double x);
+long double cubeDerivative(long double x);
+/* Swish */
+long double swish(long double x);
+long double swishDerivative(long double x);
+/* Softsign */
+long double softsign(long double x);
+long double softsignDerivative(long double x);
+/* Softplus */
+long double softplus(long double x);
+long double softplusDerivative(long double x);
+/* Softmax*/
+long double* softmax(long double* data, const int count);
+Matrix& softmaxDerivative(Matrix& matrix);
 
+
+/* Matrix iterator consumers */
+const auto SIGMOID_CONSUMER = [](long double x) -> long double { return sigmoid(x); };
+const auto SIGMOID_DERIVATIVE_CONSUMER = [](long double x) -> long double { return sigmoidDerivative(x); };
+const auto RELU_CONSUMER = [](long double x) -> long double { return relu(x); };
+const auto RELU_DERIVATIVE_CONSUMER = [](long double x) -> long double { return reluDerivative(x); };
+const auto LEAKY_RELU_CONSUMER = [](long double x) -> long double { return leakyRelu(x); };
+const auto LEAKY_RELU_DERIVATIVE_CONSUMER = [](long double x) -> long double { return leakyReluDerivative(x); };
+const auto RELU6_CONSUMER = [](long double x) -> long double { return relu6(x); };
+const auto RELU6_DERIVATIVE_CONSUMER = [](long double x) -> long double { return relu6Derivative(x); };
+const auto ELU_CONSUMER = [](long double x) -> long double { return elu(x); };
+const auto ELU_DERIVATIVE_CONSUMER = [](long double x) -> long double { return eluDerivative(x); };
+const auto SELU_CONSUMER = [](long double x) -> long double { return selu(x); };
+const auto SELU_DERIVATIVE_CONSUMER = [](long double x) -> long double { return seluDerivative(x); };
+const auto GELU_CONSUMER = [](long double x) -> long double { return gelu(x); };
+const auto GELU_DERIVATIVE_CONSUMER = [](long double x) -> long double { return geluDerivative(x); };
+const auto CUBE_CONSUMER = [](long double x) -> long double { return cube(x); };
+const auto CUBE_DERIVATIVE_CONSUMER = [](long double x) -> long double { return cubeDerivative(x); };
+const auto SWISH_CONSUMER = [](long double x) -> long double { return swish(x); };
+const auto SWISH_DERIVATIVE_CONSUMER = [](long double x) -> long double { return swishDerivative(x); };
+const auto SOFTSIGN_CONSUMER = [](long double x) -> long double { return softsign(x); };
+const auto SOFTSIGN_DERIVATIVE_CONSUMER = [](long double x) -> long double { return softsignDerivative(x); };
+const auto SOFTPLUS_CONSUMER = [](long double x) -> long double { return softplus(x); };
+const auto SOFTPLUS_DERIVATIVE_CONSUMER = [](long double x) -> long double { return softplusDerivative(x); };
 #endif
